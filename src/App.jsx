ad777bd91c23b08c189c0baa03bcc41dc2f643fa
lucas <lucas.chorolqui@gmail.com>
2023-10-25 19:49:52 -0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -12,11 +13,12 @@ import PageNotFound from "./pages/PageNotFound";
 import Cabins from "./pages/Cabins";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
@@ -43,6 +45,26 @@ function App() {
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
+
+      {/* react hot toaster library */}
+      <Toaster
+        position="top-center"
+        glutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { CabinsTable } from "../features/cabins/CabinTable";
+import { useState } from "react";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowFrom] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -12,6 +17,12 @@ function Cabins() {
 
       <Row>
         <CabinsTable />
+
+        <Button onClick={() => setShowFrom((show) => !show)}>
+          Add new cabin
+        </Button>
+
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
