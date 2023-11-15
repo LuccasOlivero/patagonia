0b334a4a4e7198dfc26092bb9edb9f3c636dd9bf
lucas <lucas.chorolqui@gmail.com>
2023-11-14 21:33:02 -0300

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -32,17 +32,7 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="dashboard" />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="bookings" element={<Bookings />} />
-            <Route path="bookings/:bookingId" element={<Booking />} />
-            <Route path="checkin/:bookingId" element={<Checkin />} />
-            <Route path="cabins" element={<Cabins />} />
-            <Route path="users" element={<Users />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="account" element={<Account />} />
-          </Route>
+          <Route element={<AppLayout />}></Route>
 
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
