aef537a68b6c2a5abab9c81813c5f68807e43d0c
lucas <lucas.chorolqui@gmail.com>
2023-11-15 22:06:58 -0300

    updates

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -29,18 +29,7 @@ const Box = styled.div`
 function PageNotFound() {
   const moveBack = useMoveBack();
 
-  return (
-    <StyledPageNotFound>
-      <Box>
-        <Heading as="h1">
-          The page you are looking for could not be found 😢
-        </Heading>
-        <button onClick={moveBack} size="large">
-          &larr; Go back
-        </button>
-      </Box>
-    </StyledPageNotFound>
-  );
+  return <StyledPageNotFound></StyledPageNotFound>;
 }
 
 export default PageNotFound;
